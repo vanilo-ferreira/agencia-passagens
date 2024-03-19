f8013867f2ade7377ce05ce5b78aefef3016009f
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-03-18 22:34:43 -0300

    feat: Adicionar arquivo passagem.py com classes relacionadas a listagem e adição de passagens

---
+++ passagem.py
@@ -0,0 +1,18 @@
+class PassagensAereasManager():
+        passagens_compradas = []
+        
+        def adicionar_passagem(self, passagem):
+            self.passagens_compradas.append(passagem)
+            
+        def listar_passagens(self):
+            return self.passagens_compradas
+            
+            
+class Passagem: 
+    def __init__(self, origem, destino, preco):
+        self.origem = origem
+        self.destino = destino
+        self.preco = preco
+        
+    def __repr__(self):
+        return  f"{self.origem} {self.destino} {self.preco}"
